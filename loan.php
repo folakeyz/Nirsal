9701d0bdab1822d2944ff864d3d165addd378bc5
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-24 17:59:48 +0100

    Update loan.php

--- loan.php
+++ loan.php
@@ -1,15 +1,25 @@
-<?php require'inc/head.php';
+<?php 
+session_start();
+require'inc/head.php';
 
-if(isset($_GET)){
-    
-$bvn = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);  
+if(isset($_SESSION['bvn'])){
+$bvn =  $_SESSION['bvn'];
  $tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn'";
-$getResults= sqlsrv_query($conn, $tsql);
+$params = array();
+$options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
+    $getResults= sqlsrv_query($conn, $tsql, $params, $options);
+    $count=sqlsrv_num_rows($getResults);
     
-    
+}elseif($count == 0){
+     /*echo'<script>
+    window.location.href="https://covid19.nmfb.com.ng/";
+    </script>';  */
+}
 
-    
-    
+else{
+    /*  echo'<script>
+    window.location.href="https://covid19.nmfb.com.ng/";
+    </script>'; */
 }
     
 
@@ -19,7 +29,7 @@ $getResults= sqlsrv_query($conn, $tsql);
 </div>
 <div class="loan-body">
     <?php
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    $row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC) 
     ?>
     <h3 class="text-success text-center"><b>WELCOME&nbsp;&nbsp;<?=$row['Applicant Name'];?></b> </h3>
    
@@ -30,7 +40,7 @@ $getResults= sqlsrv_query($conn, $tsql);
      <small>Guarantor's Info</small><hr>
                     <div class="col-md-4 form-group float-left">
            <label>Applicant's Name</label>
-           <input type="text" class="form-control" value="<?=$row['Applicant Name'];?>" name="gfname" required>
+           <input type="text" class="form-control" value="<?=$row['Applicant Name'];?>" readonly>
        </div>   
         
        <div class="col-md-4 form-group float-left">
@@ -40,22 +50,22 @@ $getResults= sqlsrv_query($conn, $tsql);
         $num =$row['Loan Amount'];
          $test=(int)$num;
         echo number_format($test);
-        ?>" name="gmobile" required>
+        ?>" name="gmobile" readonly>
        </div> 
        <div class="col-md-4 form-group float-left">
            <label>Loan Tenor</label>
-           <input type="text" class="form-control" value="<?=$row['Approved Loan Tenor'];?>" name="gemail" >
+           <input type="text" class="form-control" value="<?=$row['Approved Loan Tenor'];?>" readonly>
        </div> 
         <div class="col-md-4 form-group float-left">
            <label>Loan Moratorium</label>
-           <input type="text" class="form-control" value="<?=$row['Approved Loan Moratorium'];?>" name="bvn" required>
+           <input type="text" class="form-control" value="<?=$row['Approved Loan Moratorium'];?>" readonly>
        </div> 
         <div class="col-md-4 form-group float-left">
         <br>
-           <input type="submit" class="btn btn-success btn-block" value="Proceed" name="submit" required>
+            <a class="btn btn-success btn-block" href="offer_and_aggrement.php">Proceed</a>
        </div> 
 </div>
-    <?php } ?>
+ 
     </form>
         
 </div>  
