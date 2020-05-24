46ea7919ea28ce3a6b9ff1a45d58f2b726949117
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-24 17:59:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,65 +10,22 @@ $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
     $count=sqlsrv_num_rows($getResults);
+  echo'<script>
+    window.location.href="loan.php";
+    </script>';  
+
     
 }elseif($count == 0){
      echo'<script>
     window.location.href="https://covid19.nmfb.com.ng/";
-    </script>';  
+    </script>';
 }
 
 else{
-    echo'<script>
+      echo'<script>
     window.location.href="https://covid19.nmfb.com.ng/";
-    </script>'; 
+    </script>';
 }
     
 
 ?>
-<div class="navigation">
-    <img src="img/unnamed.png" alt="Nirsal Logo">
-</div>
-<div class="loan-body">
-    <?php
-    $row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC) 
-    ?>
-    <h3 class="text-success text-center"><b>WELCOME&nbsp;&nbsp;<?=$row['Applicant Name'];?></b> </h3>
-   
-<!--    <small>User Information</small><hr>-->
-    <form method="" action="">
-
-<div class="guarantor">
-     <small>Guarantor's Info</small><hr>
-                    <div class="col-md-4 form-group float-left">
-           <label>Applicant's Name</label>
-           <input type="text" class="form-control" value="<?=$row['Applicant Name'];?>" readonly>
-       </div>   
-        
-       <div class="col-md-4 form-group float-left">
-           <label>Approved Loan Amount</label>
-           <input type="text" class="form-control" value="&#x20A6;&nbsp;<?php
-        
-        $num =$row['Loan Amount'];
-         $test=(int)$num;
-        echo number_format($test);
-        ?>" name="gmobile" readonly>
-       </div> 
-       <div class="col-md-4 form-group float-left">
-           <label>Loan Tenor</label>
-           <input type="text" class="form-control" value="<?=$row['Approved Loan Tenor'];?>" readonly>
-       </div> 
-        <div class="col-md-4 form-group float-left">
-           <label>Loan Moratorium</label>
-           <input type="text" class="form-control" value="<?=$row['Approved Loan Moratorium'];?>" readonly>
-       </div> 
-        <div class="col-md-4 form-group float-left">
-        <br>
-            <a class="btn btn-success btn-block" href="offer_and_aggrement.php">Proceed</a>
-       </div> 
-</div>
- 
-    </form>
-        
-</div>  
-
-<?php require'inc/footer.php';?>
