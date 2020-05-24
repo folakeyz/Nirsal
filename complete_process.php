6b2e5680bfb16a8d3c54883d055290d2c2226fda
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-24 18:36:04 +0100

    Update complete_process.php

--- complete_process.php
+++ complete_process.php
@@ -5,9 +5,10 @@
     <img src="img/unnamed.png" alt="Nirsal Logo">
 </div>
 <div class="loan-body">
-<div class="transfer">
-  <form>
+<div class="transfer" id="transfer">
+  <form method="post" id="val">
     <h4 class="text-success"><b>Would you like to transfer your Loan to another Bank</b></h4><hr>
+    <p id="result"></p>
 <div class="form-group col-md-6">
  <p class="text-success"><input type="radio" name="status" value="Yes" required> Yes </p>  
 </div>
@@ -17,10 +18,53 @@
 <div class="form-group col-md-6">
 <input type="submit" name="submit" class="btn btn-success" value="Submit"> 
 </div>
+
 </form>  
+
 </div>
-
-
+<div class="btransfer" id="btransfer">
+    <form>
+        <h4 class="text-success"><b>Enter Bank and Account Number</b></h4><hr> 
+       <div class="form-group col-md-6">
+       <label>Bank Name</label>
+       <input type="text" name="bname" class="form-control" value=""> 
+        </div> 
+        <div class="form-group col-md-6">
+       <label>Account Number</label>
+       <input type="text" name="bnumber" class="form-control" value=""> 
+        </div> 
+        <div class="form-group col-md-6">
+<input type="submit" name="submit" class="btn btn-success" value="Submit"> 
+</div>
+    </form>
+      <script>
+        $(document).ready(function(){
+            //$('#create').click(function(event){
+            $("form#val").submit(function(e) {
+                event.preventDefault();
+                //var formData = $('#deploy').serialize();    
+                  var formData = new FormData(this);
+                //console.log(formData);
+                
+                 $.ajax({
+        url: 'inc/bank_update',
+        type: 'POST',
+        data: formData,
+        success: function (result) {
+        $('#result').html(result);
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+                
+               
+                    
+                });
+            });
+            
+        </script>
+</div>
 
 
 </div>
