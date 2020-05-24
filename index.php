58b8dbd1740dd2e6d242f0f66545b89e8c0f8ec7
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-24 17:31:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,61 @@
-<?php require'inc/head.php';?>
+<?php require'inc/head.php';
+
+if(isset($_GET['id'])){
+$bvn = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);  
+ $tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn'";
+$getResults= sqlsrv_query($conn, $tsql);
+}else{
+    echo'<script>
+    window.location.href="https://covid19.nmfb.com.ng/";
+    </script>';
+}
+    
+
+?>
 <div class="navigation">
     <img src="img/unnamed.png" alt="Nirsal Logo">
 </div>
-<div class="landing">
-     <div class="col-md-6 con-right text-center">
-     <div class="col-md-12 mx-auto text-center">
-      <h2 class="text-success">Targeted Credit Facility for COVID-19</h2><hr>
-    <h3 class="text-success text-center">Welcome</h3>
-    <small>Please Enter your BVN</small><hr>
-    <p id="result"></p>
-    <form method="post" action="" id="validation">
-        <div class="form-group col-md-12">
-            <input type="number" class="form-control" name="bvn" placeholder="Enter BVN" maxlength="11">
-        </div>
-            <div class="form-group col-md-12">
-            <input type="submit" class="btn btn-success curved btn-block" name="validate" value="Validate BVN">
-        </div>
-    </form>
-                
-                 <script>
-        $(document).ready(function(){
-            //$('#create').click(function(event){
-            $("form#validation").submit(function(e) {
-                event.preventDefault();
-                //var formData = $('#deploy').serialize();    
-                  var formData = new FormData(this);
-                //console.log(formData);
-                
-                 $.ajax({
-        url: 'inc/validation.php',
-        type: 'POST',
-        data: formData,
-        success: function (result) {
-        $('#result').html(result);
-        },
-        cache: false,
-        contentType: false,
-        processData: false
-    });
-                
-               
-                    
-                });
-            });
-            
-        </script>
-</div>  
+<div class="loan-body">
+    <?php
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    ?>
+    <h3 class="text-success text-center"><b>WELCOME&nbsp;&nbsp;<?=$row['Applicant Name'];?></b> </h3>
+   
+<!--    <small>User Information</small><hr>-->
+    <form method="" action="">
+
+<div class="guarantor">
+     <small>Guarantor's Info</small><hr>
+                    <div class="col-md-4 form-group float-left">
+           <label>Applicant's Name</label>
+           <input type="text" class="form-control" value="<?=$row['Applicant Name'];?>" name="gfname" required>
+       </div>   
+        
+       <div class="col-md-4 form-group float-left">
+           <label>Approved Loan Amount</label>
+           <input type="text" class="form-control" value="&#x20A6;&nbsp;<?php
+        
+        $num =$row['Loan Amount'];
+         $test=(int)$num;
+        echo number_format($test);
+        ?>" name="gmobile" required>
+       </div> 
+       <div class="col-md-4 form-group float-left">
+           <label>Loan Tenor</label>
+           <input type="text" class="form-control" value="<?=$row['Approved Loan Tenor'];?>" name="gemail" >
+       </div> 
+        <div class="col-md-4 form-group float-left">
+           <label>Loan Moratorium</label>
+           <input type="text" class="form-control" value="<?=$row['Approved Loan Moratorium'];?>" name="bvn" required>
+       </div> 
+        <div class="col-md-4 form-group float-left">
+        <br>
+           <input type="submit" class="btn btn-success btn-block" value="Proceed" name="submit" required>
+       </div> 
 </div>
-    </div> 
+    <?php } ?>
+    </form>
+        
+</div>  
 
 <?php require'inc/footer.php';?>
-   
