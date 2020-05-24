27bca6937d7ee2f0b53fe2c9d8ef8c8861df1361
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-24 21:38:07 +0100

    Update loan.php

--- SME/loan.php
+++ SME/loan.php
@@ -1,10 +1,10 @@
 <?php 
 session_start();
 require'inc/head.php';
-
 if(isset($_SESSION['bvn'])){
 $bvn =  $_SESSION['bvn'];
- $tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn'";
+//$bvn=22163440695;
+ $tsql= "SELECT * FROM [SME Loan Application Form] WHERE [Director's BVN]='$bvn' OR [Promoter's BVN]='$bvn'";
 $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
@@ -31,16 +31,16 @@ else{
     <?php
     $row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC) 
     ?>
-    <h3 class="text-success text-center"><b>WELCOME&nbsp;&nbsp;<?=$row['Applicant Name'];?></b> </h3>
+    <h3 class="text-success text-center"><b>WELCOME&nbsp;&nbsp;<?=$row['Business/Company Name'];?></b> </h3>
    
 <!--    <small>User Information</small><hr>-->
     <form method="" action="">
 
 <div class="guarantor">
-     <small>Guarantor's Info</small><hr>
+
                     <div class="col-md-4 form-group float-left">
-           <label>Applicant's Name</label>
-           <input type="text" class="form-control" value="<?=$row['Applicant Name'];?>" readonly>
+           <label>Bussiness Name</label>
+           <input type="text" class="form-control" value="<?=$row['Business/Company Name'];?>" readonly>
        </div>   
         
        <div class="col-md-4 form-group float-left">
