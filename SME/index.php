f4af8dd7295afadb1648414d776bcd38ad474aa2
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-24 21:32:17 +0100

    Update index.php

--- SME/index.php
+++ SME/index.php
@@ -1,1 +1,32 @@
 
+<?php 
+session_start();
+require'inc/head.php';
+
+if(isset($_GET['id'])){
+$bvn = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);  
+$_SESSION['bvn']=$bvn;
+ $tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn'";
+$params = array();
+$options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
+    $getResults= sqlsrv_query($conn, $tsql, $params, $options);
+    $count=sqlsrv_num_rows($getResults);
+  echo'<script>
+    window.location.href="loan.php";
+    </script>';  
+
+    
+}elseif($count == 0){
+     echo'<script>
+    window.location.href="https://covid19.nmfb.com.ng/";
+    </script>';
+}
+
+else{
+      echo'<script>
+    window.location.href="https://covid19.nmfb.com.ng/";
+    </script>';
+}
+    
+
+?>
