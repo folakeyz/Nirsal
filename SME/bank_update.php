570bebef5f43ab020da0b21ab395ec9988848873
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-25 16:26:16 +0100

    Update bank_update.php

--- SME/bank_update.php
+++ SME/bank_update.php
@@ -23,7 +23,10 @@ else{
 }
     
 ?>
-
+<style>
+ .transfer{background:url(img/covid.jpg);}
+  .btransfer{background:url(img/covid.jpg);}
+</style>
 
 <div class="navigation">
     <img src="img/unnamed.png" alt="Nirsal Logo">
