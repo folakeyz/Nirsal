089711615967f75e8cfd624c5505d014a107324b
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-26 00:14:33 +0100

    Update offer_and_aggrement.php

--- SME/offer_and_aggrement.php
+++ SME/offer_and_aggrement.php
@@ -5,7 +5,7 @@ require'inc/head.php';
 
 if(isset($_SESSION['bvn'])){
 $bvn =  $_SESSION['bvn'];
-$tsql= "SELECT * FROM [SME Loan Application Form] WHERE [Director's BVN]='$bvn' OR [Promoter's BVN]='$bvn'";
+$tsql= "SELECT * FROM [SME Loan Application Form] WHERE [Director's BVN]='$bvn' OR [Promoter's BVN]='$bvn' AND [ApprovalStatus]='APPROVED'";
 $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
