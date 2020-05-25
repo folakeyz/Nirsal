50b2eb4439e9c05e9760cb1eadd17ada2dae3f2d
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-25 17:38:07 +0100

    Update agree.php

--- inc/agree.php
+++ inc/agree.php
@@ -1,7 +1,20 @@
 <?php
+require'dbh.inc.php';
 if(isset($_POST)){
+        $agree="Accepted";
+        $bvn = $_POST['bvn'];
+        $tsql= "UPDATE [GuarantorsForms] SET Decision='$agree' WHERE ApplicantBvn='$bvn'";
+$getResults= sqlsrv_query($conn, $tsql);
+
+    if($getResults){
         echo'<script>
+        swal("success!", "Terms and Conditions Accepted!", "success");
        window.location.href="complete_process.php";
         </script>';
+        }else{
+           echo'<script>
+      swal("Warning!", "An Error Occured,'.$bvn.'Please Try again!", "warning");
+        </script>';      
+        }
     
 }
