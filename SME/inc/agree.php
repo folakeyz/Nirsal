1d7835e9b193302ced1a40f3afe154acf28a1e32
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-25 17:16:30 +0100

    Update agree.php

--- SME/inc/agree.php
+++ SME/inc/agree.php
@@ -1,19 +1,12 @@
 <?php
+require'dbh.inc.php';
 if(isset($_POST)){
         $agree="Accepted";
         $bvn = $_POST['bvn'];
         $tsql= "UPDATE [SmeGuarantors] SET Decision='$agree' WHERE ApplicantBvn='$bvn'";
 $getResults= sqlsrv_query($conn, $tsql);
-        $tsql= "SELECT * FROM [SmeGuarantors] WHERE ApplicantBvn='$bvn'";
-$params = array();
-$options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
-    $getResults= sqlsrv_query($conn, $tsql, $params, $options);
-    $count=sqlsrv_num_rows($getResults);
-        echo $count;
-        
-        
-        
-       /* if($getResults){
+
+    if($getResults){
         echo'<script>
         swal("success!", "Terms and Conditions Accepted!", "success");
        window.location.href="complete_process.php";
@@ -22,7 +15,7 @@ $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
            echo'<script>
       swal("Warning!", "An Error Occured,'.$bvn.'Please Try again!", "warning");
         </script>';      
-        }*/
+        }
     
 }
 
