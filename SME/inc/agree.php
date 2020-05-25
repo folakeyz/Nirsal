f7e5f72164832d58ca7fe0ca744430269ee51bc5
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-25 16:34:20 +0100

    Update agree.php

--- SME/inc/agree.php
+++ SME/inc/agree.php
@@ -1,7 +1,19 @@
 <?php
 if(isset($_POST)){
+        $agree="Accepted";
+        $bvn = filter_input(INPUT_POST, 'bvn', FILTER_SANITIZE_STRING);
+        $tsql= "UPDATE SmeGuarantors SET Decision='$agree' WHERE ApplicantBvn='$bvn'";
+$getResults= sqlsrv_query($conn, $tsql);
+        
+        if($getResults){
         echo'<script>
+        swal("success!", "Terms and Conditions Accepted!", "success");
        window.location.href="complete_process.php";
         </script>';
+        }else{
+           echo'<script>
+      swal("Warning!", "An Error Occured, Please Try again!", "warning");
+        </script>';      
+        }
     
 }
