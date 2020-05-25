91fa31660a6cfc318f933663e6230aaf402b5103
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-26 00:12:25 +0100

    Update index.php

--- SME/index.php
+++ SME/index.php
@@ -6,7 +6,7 @@ require'inc/head.php';
 if(isset($_GET['id'])){
 $bvn = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);  
 $_SESSION['bvn']=$bvn;
- $tsql= "SELECT * FROM [SME Loan Application Form] WHERE [Director's BVN]='$bvn' OR [Promoter's BVN]='$bvn'";
+ $tsql= "SELECT * FROM [SME Loan Application Form] WHERE [Director's BVN]='$bvn' OR [Promoter's BVN]='$bvn' AND [ApprovalStatus]='APPROVED'";
 $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
