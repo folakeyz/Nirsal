b2e6dc8f15556647d1e1815898ea83a12cc46292
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-05-26 00:04:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require'inc/head.php';
 if(isset($_GET['id'])){
 $bvn = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);  
 $_SESSION['bvn']=$bvn;
- $tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn'";
+ $tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn' AND [Approval Status]='APPROVED'";
 $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
