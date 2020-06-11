7a9a2c35c81e4b947515d05d9ea929719e0748a3
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-06-11 11:54:46 +0100

    Update agree.php

--- inc/agree.php
+++ inc/agree.php
@@ -26,7 +26,7 @@ $getResults= sqlsrv_query($conn, $tsql);
         }
         }else{
            echo'<script>
-        swal("success!", "Terms and Conditions Accepted!", "success");
+        swal("success!", "Accepted!", "success");
        window.location.href="complete_process.php?BIMS=BIMS";      
         }
       
