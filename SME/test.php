8efa1e32d48cd60af51359d8ddd5bad0241be302
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-06-12 15:06:59 +0100

    Update test.php

--- SME/test.php
+++ SME/test.php
@@ -3,7 +3,7 @@
 session_start();
 require'inc/head.php';
 $bvn=22141701040;
-$tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn' AND [Approval Status]='APPROVED'";
+$tsql= "SELECT * FROM [SME Loan Application Form] WHERE [Director's BVN]='$bvn' OR [Promoter's BVN]='$bvn' AND [ApprovalStatus]='APPROVED'";
 $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
