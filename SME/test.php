e7c7aa7c3f986ad8a377fb58dcffac9234b88a2e
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-06-16 14:02:49 +0100

    Update test.php

--- SME/test.php
+++ SME/test.php
@@ -8,8 +8,9 @@ $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
     $count=sqlsrv_num_rows($getResults);
-    
-    echo $count;
- $row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC);
+     $row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC);
 echo $row['ApprovalStatus'];
 echo $row['Business/Company Name'];
+    echo $count;
+
+
