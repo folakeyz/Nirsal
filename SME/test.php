cbd4a53f7a08bba82bb1878b24d6d8f8f7316815
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-06-16 14:35:07 +0100

    Update test.php

--- SME/test.php
+++ SME/test.php
@@ -4,7 +4,7 @@ session_start();
 require'inc/head.php';
 $bvn=22143025438;
 $app="APPROVED";
-$tsql= "SELECT * FROM [SME Loan Application Form] WHERE [Director's BVN]='$bvn' OR [Promoter's BVN]='$bvn' AND WHERE [ApprovalStatus]='APPROVED'";
+$tsql= "SELECT * FROM [SME Loan Application Form] WHERE [Director's BVN]='$bvn' OR [Promoter's BVN]='$bvn' AND [ApprovalStatus]='$app'";
 $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
