e2a47d0520ab7b1daba4df0bd225ae89cecf8b58
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-06-22 11:18:19 +0100

    Update test.php

--- test.php
+++ test.php
@@ -3,10 +3,15 @@ session_start();
 require'inc/head.php';
 
 $_SESSION['bvn']=22547201573;
- $tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn' AND [Approval Status]='APPROVED'";
+ $tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn'";
 $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
     $count=sqlsrv_num_rows($getResults);
+ while($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)){
+        echo $row['ApprovalStatus'].'<br>';
+            echo $row['Approved Loan Amount'].'<br>';
+
+    }
 
     echo $count;
