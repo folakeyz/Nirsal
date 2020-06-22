04021ebf6d6bb859e6be867f41cb0611319b5861
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-06-22 11:44:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,22 +5,19 @@ require'inc/head.php';
 if(isset($_GET['id'])){
 $bvn = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);  
 $_SESSION['bvn']=$bvn;
- $stat="APPROVED";
  $tsql= "SELECT * FROM [Targeted Credit Facility (TCF) - Household Loan Application Form] WHERE BVN='$bvn' AND [Approval Status] IS NOT NULL";
 $params = array();
 $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
     $count=sqlsrv_num_rows($getResults);
- echo $count;
-echo"Try again Later.";
 if($count == 0){
-     /*echo'<script>
+     echo'<script>
     window.location.href="https://covid19.nmfb.com.ng/";
-    </script>';*/
+    </script>';
 }else{
- /* echo'<script>
+ echo'<script>
     window.location.href="loan.php";
-    </script>';  */
+    </script>';  
 }
     
 }
