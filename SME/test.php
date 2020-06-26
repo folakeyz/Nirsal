cda15a2837e5a6afe8d4c621098a2ef14ebdb804
I-Codes <54380342+folakeyz@users.noreply.github.com>
2020-06-26 07:25:52 +0100

    Update test.php

--- SME/test.php
+++ SME/test.php
@@ -24,11 +24,11 @@ $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
     $getResults= sqlsrv_query($conn, $tsql, $params, $options);
     $count=sqlsrv_num_rows($getResults);
     while($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)){
-        echo .'<p> ID' .$row['id'].'<br></p>';
-        echo .'<p> Approval Status' .$row['ApprovalStatus'].'<br></p>';
-            echo .'<p> Amount' .$row['Approved Loan Amount'].'<br></p>';
-         echo .'<p> D BVN' .$row["Director's BVN"].'<br></p>';
-         echo .'<p> P BVN' .$row["Promoter's BVN"].'<br></p>';
+        echo '<p> ID' .$row['id'].'<br></p>';
+        echo '<p> Approval Status' .$row['ApprovalStatus'].'<br></p>';
+            echo '<p> Amount' .$row['Approved Loan Amount'].'<br></p>';
+         echo '<p> D BVN' .$row["Director's BVN"].'<br></p>';
+         echo '<p> P BVN' .$row["Promoter's BVN"].'<br></p>';
 
     }
   echo $count.'<br>';
